55d0e6888ee11046466a68d14fc6f0a1b638f902
Am2200 <am.200@rambler.ru>
2016-07-20 22:56:51 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -12,6 +12,10 @@ end
 
 # Сущность клиента
 class Client < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :datestamp, presence: true
+  validates :color, presence: true
 end
 
 #Сущность парикмахеров
